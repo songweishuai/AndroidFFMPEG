76303646c28cbe998089c3fe08a4303e7ee0e2a0
songweishuai <240948380@qq.com>
2020-08-21 14:11:31 +0800

    CMakeList调用ndk系统库libandroid.so

--- ffmpeg/src/main/jni/player.cpp
+++ ffmpeg/src/main/jni/player.cpp
@@ -1,30 +1,144 @@
-//
-// Created by keda on 20-8-20.
-//
-
 #include <stdio.h>
 #include <signal.h>
 
 #include "player.h"
+#include "include/libavformat/avformat.h"
+#include "include/libavutil/dict.h"
+#include "include/libavcodec/avcodec.h"
 #include "include/libavutil/log.h"
-#include "../../../../../../../IDE/Android/Sdk/ndk/android-ndk-r14b/platforms/android-19/arch-arm/usr/include/stdlib.h"
-#include "../../../../../../../IDE/Android/Sdk/ndk/android-ndk-r14b/toolchains/llvm/prebuilt/linux-x86_64/lib64/clang/3.8.275480/include/stdarg.h"
-#include "../../../../../../../IDE/Android/Sdk/ndk/android-ndk-r14b/platforms/android-19/arch-arm/usr/include/android/log.h"
+#include "include/libavfilter/avfilter.h"
 
+#define TEST_FILE_TFCARD "/mnt/extSdCard/clear.ts"
 
 GlobalContext global_context;
 
 static void sigterm_handler(int sig) {
-    av_log(NULL, AV_LOG_ERROR, "");
-    exit(123);
+	av_log(NULL, AV_LOG_ERROR, "sigterm_handler : sig is %d \n", sig);
+	exit(123);
 }
 
-static void ffmpeg_log_callback(void *ptr, int level, const char *fmt, va_list vl)
-{
-    __android_log_vprint(ANDROID_LOG_DEBUG, "FFmpeg", fmt, vl);
+static void ffmpeg_log_callback(void *ptr, int level, const char *fmt,va_list vl) {
+	__android_log_vprint(ANDROID_LOG_DEBUG, "FFmpeg", fmt, vl);
 }
 
-void *open_media(void *argv) {
+void* open_media(void *argv) {
+	int i;
+	int err = 0;
+	int framecnt;
+	AVFormatContext *fmt_ctx = NULL;
+	AVDictionaryEntry *dict = NULL;
+	AVPacket pkt;
+	int video_stream_index = -1;
+	pthread_t thread;
 
-    av_log_set_callback();
+	global_context.quit = 0;
+	global_context.pause = 0;
+
+	// register INT/TERM signal
+	signal(SIGINT, sigterm_handler); /* Interrupt (ANSI).    */
+	signal(SIGTERM, sigterm_handler); /* Termination (ANSI).  */
+
+	av_log_set_callback(ffmpeg_log_callback);
+
+	// set log level
+	av_log_set_level(AV_LOG_WARNING);
+
+	/* register all codecs, demux and protocols */
+	avfilter_register_all();
+	av_register_all();
+	avformat_network_init();
+
+	fmt_ctx = avformat_alloc_context();
+
+	err = avformat_open_input(&fmt_ctx, TEST_FILE_TFCARD, NULL, NULL);
+	if (err < 0) {
+		char errbuf[64];
+		av_strerror(err, errbuf, 64);
+		av_log(NULL, AV_LOG_ERROR, "avformat_open_input : err is %d , %s\n",
+				err, errbuf);
+		err = -1;
+		goto failure;
+	}
+
+	if ((err = avformat_find_stream_info(fmt_ctx, NULL)) < 0) {
+		av_log(NULL, AV_LOG_ERROR, "avformat_find_stream_info : err is %d \n",
+				err);
+		err = -1;
+		goto failure;
+	}
+
+	// search video stream in all streams.
+	for (i = 0; i < fmt_ctx->nb_streams; i++) {
+		// because video stream only one, so found and stop.
+		if (fmt_ctx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+			video_stream_index = i;
+			break;
+		}
+	}
+
+	// if no video and audio, exit
+	if (-1 == video_stream_index) {
+		goto failure;
+	}
+
+	// open video
+	if (-1 != video_stream_index) {
+		global_context.vcodec_ctx = fmt_ctx->streams[video_stream_index]->codec;
+		global_context.vstream = fmt_ctx->streams[video_stream_index];
+		global_context.vcodec = avcodec_find_decoder(
+				global_context.vcodec_ctx->codec_id);
+		if (NULL == global_context.vcodec) {
+			av_log(NULL, AV_LOG_ERROR,
+					"avcodec_find_decoder video failure. \n");
+			goto failure;
+		}
+
+		if (avcodec_open2(global_context.vcodec_ctx, global_context.vcodec,
+				NULL) < 0) {
+			av_log(NULL, AV_LOG_ERROR, "avcodec_open2 failure. \n");
+			goto failure;
+		}
+
+		if ((global_context.vcodec_ctx->width > 0)
+				&& (global_context.vcodec_ctx->height > 0)) {
+
+			setBuffersGeometry(global_context.vcodec_ctx->width,
+					global_context.vcodec_ctx->height);
+		}
+
+		av_log(NULL, AV_LOG_ERROR, "video : width is %d, height is %d . \n",
+				global_context.vcodec_ctx->width,
+				global_context.vcodec_ctx->height);
+
+	}
+
+	if (-1 != video_stream_index) {
+		pthread_create(&thread, NULL, video_thread, NULL);
+	}
+
+	// read url media data circle
+	while ((av_read_frame(fmt_ctx, &pkt) >= 0) && (!global_context.quit)) {
+		if (pkt.stream_index == video_stream_index) {
+			packet_queue_put(&global_context.video_queue, &pkt);
+		} else {
+			av_free_packet(&pkt);
+		}
+	}
+
+	// wait exit
+	while (!global_context.quit) {
+		usleep(1000);
+	}
+
+	failure:
+
+	if (fmt_ctx) {
+		avformat_close_input(&fmt_ctx);
+		avformat_free_context(fmt_ctx);
+	}
+
+	avformat_network_deinit();
+
+	return 0;
 }
+
--- ffmpeg/src/main/jni/surface.cpp
+++ ffmpeg/src/main/jni/surface.cpp
@@ -1,17 +1,12 @@
-//
-// Created by keda on 20-8-20.
-//
-
-#include <pthread.h>
 #include "com_example_ffmpeg_VideoSurface.h"
+#include "player.h"
 
+// for native window JNI
 #include <android/native_window_jni.h>
 #include <android/native_window.h>
-#include <android/log.h>
-
 
 static ANativeWindow *mANativeWindow;
-static ANativeWindow_Buffer mWBuffer;
+static ANativeWindow_Buffer nwBuffer;
 
 static jclass globalVideoSurfaceClass = NULL;
 static jobject globalVideoSurfaceObject = NULL;
@@ -19,38 +14,94 @@ static jobject globalVideoSurfaceObject = NULL;
 extern "C" {
 
 
-/*
- * Class:     com_example_ffmpeg_VideoSurface
- * Method:    setSurface
- * Signature: (Landroid/view/Surface;)I
- */
-JNIEXPORT jint JNICALL
-Java_com_example_ffmpeg_VideoSurface_setSurface(JNIEnv *env, jobject obj, jobject surface) {
-    pthread_t thread_1;
+void renderSurface(uint8_t *pixel) {
 
-    jclass localVideoSurfaceClass = env->FindClass("com/example/ffmpeg/VideoSurface");
-    if (NULL == localVideoSurfaceClass) {
-        //__android_log_vprint("")
+    if (global_context.pause) {
+        return;
+    }
+
+    ANativeWindow_acquire(mANativeWindow);
+
+    if (0 != ANativeWindow_lock(mANativeWindow, &nwBuffer, NULL)) {
+        LOGV("ANativeWindow_lock() error");
+        return;
+    }
+    //LOGV("renderSurface, %d, %d, %d", nwBuffer.width ,nwBuffer.height, nwBuffer.stride);
+    if (nwBuffer.width >= nwBuffer.stride) {
+        //srand(time(NULL));
+        //memset(piexels, rand() % 100, nwBuffer.width * nwBuffer.height * 2);
+        //memcpy(nwBuffer.bits, piexels, nwBuffer.width * nwBuffer.height * 2);
+        memcpy(nwBuffer.bits, pixel, nwBuffer.width * nwBuffer.height * 2);
+    } else {
+        LOGV("new buffer width is %d,height is %d ,stride is %d",
+             nwBuffer.width, nwBuffer.height, nwBuffer.stride);
+        int i;
+        for (i = 0; i < nwBuffer.height; ++i) {
+            memcpy((void *) ((int) nwBuffer.bits + nwBuffer.stride * i * 2),
+                   (void *) ((int) pixel + nwBuffer.width * i * 2),
+                   nwBuffer.width * 2);
+        }
+    }
+
+    if (0 != ANativeWindow_unlockAndPost(mANativeWindow)) {
+        LOGV("ANativeWindow_unlockAndPost error");
+        return;
+    }
+
+    ANativeWindow_release(mANativeWindow);
+}
+
+// format not used now.
+int32_t setBuffersGeometry(int32_t width, int32_t height) {
+    int32_t format = WINDOW_FORMAT_RGB_565;
+
+    if (NULL == mANativeWindow) {
+        LOGV("mANativeWindow is NULL.");
         return -1;
     }
 
-    globalVideoSurfaceClass = (jclass) env->NewGlobalRef(localVideoSurfaceClass);
+    return ANativeWindow_setBuffersGeometry(mANativeWindow, width, height,
+                                            format);
+}
 
+// set the surface
+/*
+ * Class:     com_player_ffmpeg_VideoSurface
+ * Method:    setSurface
+ * Signature: (Landroid/view/Surface;)I
+ */JNIEXPORT jint JNICALL Java_com_example_ffmpeg_VideoSurface_setSurface(
+        JNIEnv *env, jobject obj, jobject surface) {
+
+    pthread_t thread_1;
+
+    //LOGV("fun env is %p", env);
+
+    jclass localVideoSurfaceClass = env->FindClass("com/example/ffmpeg/VideoSurface");
+    if (NULL == localVideoSurfaceClass) {
+        LOGV("FindClass VideoSurface failure.");
+        return -1;
+    }
+
+    globalVideoSurfaceClass = (jclass) env->NewGlobalRef(
+            localVideoSurfaceClass);
     if (NULL == globalVideoSurfaceClass) {
-
+        LOGV("localVideoSurfaceClass to globalVideoSurfaceClass failure.");
     }
 
     globalVideoSurfaceObject = (jclass) env->NewGlobalRef(obj);
     if (NULL == globalVideoSurfaceObject) {
-
+        LOGV("obj to globalVideoSurfaceObject failure.");
     }
 
     if (NULL == surface) {
+        LOGV("surface is null, destroy?");
         mANativeWindow = NULL;
         return 0;
     }
 
+    // obtain a native window from a Java surface
     mANativeWindow = ANativeWindow_fromSurface(env, surface);
+    LOGV("mANativeWindow ok");
 
     pthread_create(&thread_1, NULL, open_media, NULL);
 
@@ -58,36 +109,32 @@ Java_com_example_ffmpeg_VideoSurface_setSurface(JNIEnv *env, jobject obj, jobjec
 }
 
 /*
- * Class:     com_example_ffmpeg_VideoSurface
- * Method:    nativePausePlayer
+ * Class:     com_player_ffmpeg_VideoSurface
+ * Method:    onPause
  * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_com_example_ffmpeg_VideoSurface_nativePausePlayer(JNIEnv *env, jobject)
-{
+ */JNIEXPORT jint JNICALL Java_com_example_ffmpeg_VideoSurface_nativePausePlayer(JNIEnv *,
+                                                                                 jobject) {
     global_context.pause = 1;
-    return 1;
+    return 0;
 }
 
 /*
- * Class:     com_example_ffmpeg_VideoSurface
- * Method:    nativeResumePlayer
+ * Class:     com_player_ffmpeg_VideoSurface
+ * Method:    onResume
  * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_com_example_ffmpeg_VideoSurface_nativeResumePlayer(JNIEnv *env, jobject)
-{
+ */JNIEXPORT jint JNICALL Java_com_example_ffmpeg_VideoSurface_nativeResumePlayer(JNIEnv *,
+                                                                                  jobject) {
     global_context.pause = 0;
-    return 1;
+    return 0;
 }
 
 /*
- * Class:     com_example_ffmpeg_VideoSurface
- * Method:    nativeStopPlayer
+ * Class:     com_player_ffmpeg_VideoSurface
+ * Method:    onDestroy
  * Signature: ()I
- */
-JNIEXPORT jint JNICALL Java_com_example_ffmpeg_VideoSurface_nativeStopPlayer(JNIEnv *env, jobject)
-{
+ */JNIEXPORT jint JNICALL Java_com_example_ffmpeg_VideoSurface_nativeStopPlayer(JNIEnv *,
+                                                                                jobject) {
     global_context.quit = 1;
-    return 1;
+    return 0;
 }
-
 }
